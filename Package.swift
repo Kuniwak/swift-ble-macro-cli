d1f5fae763d1e3ab8ec43f4284a34f4efa084cc8
Kuniwak <yuki.kokubun@dena.com>
2024-06-15 14:46:46 +0900

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", .upToNextMajor(from: "1.2.0")),
         .package(url: "https://github.com/xcode-actions/swift-signal-handling", .upToNextMajor(from: "1.0.0")),
-        .package(url: "https://github.com/Kuniwak/swift-ble-macro.git", .upToNextMajor(from: "2.0.1")),
+        .package(url: "https://github.com/Kuniwak/swift-ble-macro.git", .upToNextMajor(from: "2.1.1")),
         .package(url: "https://github.com/Kuniwak/swift-logger.git", .upToNextMajor(from: "1.1.0")),
         .package(url: "https://github.com/Kuniwak/swift-ble-assigned-numbers", .upToNextMajor(from: "2.0.0")),
     ],
